ec4f08c8f1f9062bc600525048137f0c7a42b927
Korablin Ivan <wan6sta@gmail.com>
2022-10-08 18:54:21 +0300

    Decomposed the webpack config.

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,25 @@
+import {BuildOptions} from './types/config'
+import webpack from 'webpack';
+import {buildPlugins} from './buildPlugins';
+import {buildLoaders} from './buildLoaders';
+import {buildResolvers} from './buildResolvers';
+
+export function BuildWebpackConfig(options: BuildOptions): webpack.Configuration {
+
+  const {mode, paths} = options
+
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: '[name].[contenthash].js',
+      path: paths.build,
+      clean: true
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders()
+    },
+    resolve: buildResolvers(),
+  }
+}
