71b7094f0cb291eccdeda2f3ff38ce6a5d776411
Korablin Ivan <wan6sta@gmail.com>
2022-10-10 17:17:22 +0300

    Added route config.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import {Suspense} from 'react'
-import {Link, Route, Routes} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import 'app/styles/index.scss'
 import {useTheme} from 'app/providers/ThemeProvider'
-import {AboutPage} from 'pages/AboutPage'
-import {MainPage} from 'pages/MainPage'
 import {cn} from 'shared/lib/classNames/cn'
+import {AppRouter} from 'app/providers/router';
 
 export const App = () => {
   const {theme, changeTheme} = useTheme()
@@ -14,12 +12,6 @@ export const App = () => {
     <Link to={'/about'}>ABOUT</Link>
 
     <button onClick={changeTheme}>Change theme</button>
-
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path={'/about'} element={<AboutPage/>}/>
-        <Route path={'/'} element={<MainPage/>}/>
-      </Routes>
-    </Suspense>
+    <AppRouter/>
   </div>
 }
