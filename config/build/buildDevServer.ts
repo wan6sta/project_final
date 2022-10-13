a83ef39ddc5ddaab4cc61096143af1816f638ffb
Korablin Ivan <wan6sta@gmail.com>
2022-10-13 20:36:31 +0300

    Added test HMR.

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -8,6 +8,7 @@ export function buildDevServer(options: BuildOptions): Configuration {
   return {
     port,
     open: true,
-    historyApiFallback: true
+    historyApiFallback: true,
+    hot: true
   }
 }
